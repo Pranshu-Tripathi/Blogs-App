58717538b3d590cae74b3496802593d2603757bc
Pranshu Tripathi <pranshu.tripathi@schrodinger.com>
2024-10-12 16:29:16 +0530

    Implementation For Client Side React Application.
    	- Problems Identified: Too many fetch calls are performed on a reload.
    	- Better option would be to have a data query service that bundles up all the data and sends to client.

---
+++ client/src/CommentCreate.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+
+const CommentCreate = ({postId}) => {
+    const [content, setContent] = useState('');
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+
+        await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+            content
+        });
+        setContent('');
+    }
+
+    return (<div className="container">
+        <form onSubmit={onSubmit}>
+            <div className="form-group">
+                <label>New Comment</label>
+                <input className="form-control" value={content} onChange={e => setContent(e.target.value)} style={{marginTop:10, marginBottom: 10}} />
+            </div>
+            <button className="btn btn-primary" style={{marginBottom:10}}>Submit</button>
+        </form>
+    </div>)
+
+}
+
+export default CommentCreate;
