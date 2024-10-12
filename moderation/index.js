ca7441d65f6fcb3467ac24586c731fbf2e4a79b6
Pranshu Tripathi <pranshu.tripathi@schrodinger.com>
2024-10-13 03:27:07 +0530

    Event Synchronization implemented.
    	- We log all the events that have occured since origin.
    	- When a service is added / comes out of down time, it will read the already occured events and regenerated the data.

--- moderation/index.js
+++ moderation/index.js
@@ -5,8 +5,7 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events', async (req, res) => {
-    const { type, data } = req.body;
+const handleEvent = async (type, data) => {
     if(type === 'CommentCreated') {
         const status = data.content.includes('orange') ? 'rejected' : 'approved';
         await axios.post('http://localhost:4005/events', {
@@ -19,9 +18,20 @@ app.post('/events', async (req, res) => {
             }
         }).catch((err)=> console.log(err));
     }
+}
+
+app.post('/events', async (req, res) => {
+    const { type, data } = req.body;
+    handleEvent(type, data);
     res.send({});
 });
 
-app.listen(4003, () => {
+app.listen(4003, async () => {
     console.log('COMMENT MODERATION SERVICE: Listening on 4003');
+
+    const res = await axios.get('http://localhost:4005/events').catch((err) => console.log(err));
+    for (let event of res.data) {
+        console.log('Processing event:', event.type);
+        handleEvent(event.type, event.data);
+    }
 });
