09bb3065c556eac3145eb53baa21547a8bb0a925
Pranshu Tripathi <pranshu.tripathi@schrodinger.com>
2024-10-13 05:37:59 +0530

    Event bus docker compose

--- event-bus/index.js
+++ event-bus/index.js
@@ -10,10 +10,10 @@ const events = [];
 app.post('/events', (req, res) => {
     const event = req.body;
     events.push(event);
-    axios.post('http://localhost:4000/events', event).catch((err) => console.log(err));
-    axios.post('http://localhost:4001/events', event).catch((err) => console.log(err));
-    axios.post('http://localhost:4002/events', event).catch((err) => console.log(err));
-    axios.post('http://localhost:4003/events', event).catch((err) => console.log(err));
+    axios.post('/posts-service/events', event).catch((err) => console.log(err));
+    axios.post('/comments-service/events', event).catch((err) => console.log(err));
+    axios.post('/query-service/events', event).catch((err) => console.log(err));
+    axios.post('/moderation-service/events', event).catch((err) => console.log(err));
 
     res.send({ status: 'OK'});
 });
